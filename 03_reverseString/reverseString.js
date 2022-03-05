023db8399e131771ee7f47da08bc4d7b503296ab
Joseph Adamo <josephaadamo67@gmail.com>
2022-03-04 19:25:19 -0600

    Completed 03-07 JS exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let temp = "";
 
+    for (let i = str.length;i>0;i--){
+        temp += str.substring(i-1,i);
+    }
+
+    return temp;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
+const ftoc = function(tempature) {
 
+  return parseFloat(((tempature-32)*5/9).toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(tempature) {
 
+  return parseFloat((tempature*9/5+32).toFixed(1));
 };
 
 // Do not edit below this line
